637e04434ae0cc8ef5dadd5d672a2d0c09504332
Christoffer Sandberg <cs@tuxedo.de>
2019-08-13 09:52:23 +0200

    Add angular material and remove bootstrap

--- src/ng-app/app/app.module.ts
+++ src/ng-app/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { NgxElectronModule } from 'ngx-electron';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,7 +15,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     BrowserModule,
     AppRoutingModule,
     NgxElectronModule,
-    NgbModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
