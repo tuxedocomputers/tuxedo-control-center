6fa85a73dfa58b78f4c3d063e99427e5e858f5c8
Christoffer Sandberg <cs@tuxedo.de>
2019-08-28 09:52:19 +0200

    Set start view to profile-manager

--- src/ng-app/app/app-routing.module.ts
+++ src/ng-app/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { CpuSettingsComponent } from './cpu-settings/cpu-settings.component';
 import { ProfileManagerComponent } from './profile-manager/profile-manager.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'cpu-settings', pathMatch: 'full' },
+  { path: '', redirectTo: 'profile-manager', pathMatch: 'full' },
   { path: 'cpu-settings', component: CpuSettingsComponent },
   { path: 'profile-manager', component: ProfileManagerComponent },
   { path: 'profile-manager/:profileName', component: ProfileManagerComponent }
