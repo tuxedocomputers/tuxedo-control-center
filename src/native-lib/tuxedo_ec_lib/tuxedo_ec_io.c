a535da32b4aa9f1040efb9367ce3813d2b629a03
Christoffer Sandberg <cs@tuxedo.de>
2019-10-23 15:35:16 +0200

    Tweaks and fixes to EC API
    - Changed waiting for ready port logic
    - Refactored various things incl. return values

--- src/native-lib/tuxedo_ec_lib/tuxedo_ec_io.c
+++ src/native-lib/tuxedo_ec_lib/tuxedo_ec_io.c
@@ -5,47 +5,63 @@
 
 #define TIMEOUT             1000000
 
-static int set_port_perm(int __turn_on)
-{
-    int result_cmd = ioperm(PORT_COMMAND, 1, __turn_on);
-    if (result_cmd != 0) return result_cmd;
-
-    int result_data = ioperm(PORT_DATA, 1, __turn_on);
-    if (result_data != 0) return result_data;
-
-    return 0;
-}
+static int set_port_perm(int);
+static int wait_for_write(unsigned short);
+static int wait_for_read(unsigned short);
 
 int init_ports()
 {
     return set_port_perm(1);
 }
 
-int deinit_ports()
+int close_ports()
 {
     return set_port_perm(0);
 }
 
 int write_command(uint8_t __cmd)
 {
-    int timeout;
-
-    for(timeout = TIMEOUT; (inb(PORT_COMMAND) & 0x01) == 0 && timeout > 0; timeout--);
-    if (timeout == 0) return -1;
-
+    if (wait_for_write(PORT_COMMAND) == EC_ERROR) return EC_ERROR;
     outb(__cmd, PORT_COMMAND);
-
-    return 0;
+    return EC_SUCCESS;
 }
 
 int write_data(uint8_t __data)
 {
-    int timeout;
-
-    for(timeout = TIMEOUT; (inb(PORT_COMMAND) & 0x02) != 0  && timeout > 0; timeout--);
-    if (timeout == 0) return -1;
-
+    if (wait_for_write(PORT_COMMAND) == EC_ERROR) return EC_ERROR;
     outb(__data, PORT_DATA);
+    return EC_SUCCESS;
+}
 
-    return 0;
+static int set_port_perm(int __turn_on)
+{
+    int result_cmd = ioperm(PORT_COMMAND, 1, __turn_on);
+    if (result_cmd != 0) return EC_ERROR;
+
+    int result_data = ioperm(PORT_DATA, 1, __turn_on);
+    if (result_data != 0) return EC_ERROR;
+
+    return EC_SUCCESS;
+}
+
+static int wait_for_write(unsigned short __port)
+{
+    int timeout;
+    for(timeout = TIMEOUT; (inb(__port) & 0x02) != 0 && timeout > 0; timeout--) sched_yield();
+    if (timeout == 0) {
+        return EC_ERROR;
+    } else {
+        return EC_SUCCESS;
+    }
+}
+
+static int wait_for_read(unsigned short __port)
+{
+    int timeout;
+    for(timeout = TIMEOUT; (inb(__port) & 0x01) == 0 && timeout > 0; timeout--) sched_yield();
+    if (timeout == 0) {
+        return EC_ERROR;
+    } else {
+        return EC_SUCCESS;
+    }
 }
