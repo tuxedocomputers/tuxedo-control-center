41d540ed4b1040cdcfafb84e2c584a5da44449b0
Christoffer Sandberg <cs@tuxedo.de>
2019-11-28 12:17:45 +0100

    Add licence header to source files & GUI info area

--- src/common/classes/CpuController.ts
+++ src/common/classes/CpuController.ts
@@ -1,3 +1,21 @@
+/*!
+ * Copyright (c) 2019 TUXEDO Computers GmbH <tux@tuxedocomputers.com>
+ *
+ * This file is part of TUXEDO Control Center.
+ *
+ * TUXEDO Control Center is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TUXEDO Control Center is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TUXEDO Control Center.  If not, see <https://www.gnu.org/licenses/>.
+ */
 import * as path from 'path';
 import { LogicalCpuController } from './LogicalCpuController';
 import { SysFsPropertyInteger, SysFsPropertyNumList } from './SysFsProperties';
