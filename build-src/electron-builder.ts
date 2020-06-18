320ec0ef0fcadb93e95757b0098c82922900de20
Christoffer Sandberg <cs@tuxedo.de>
2020-06-18 10:21:09 +0200

    Packaging: Update tuxedo-cc-wmi dependency version

--- build-src/electron-builder.ts
+++ build-src/electron-builder.ts
@@ -62,7 +62,7 @@ async function buildDeb(): Promise<void> {
             description: 'TUXEDO Control Center Application'
         },
         deb: {
-            depends: ['tuxedo-cc-wmi (>= 0.1.1)'],
+            depends: ['tuxedo-cc-wmi (>= 0.1.4)'],
             category: 'System',
             fpm: [
                 '--after-install=./build-src/after_install.sh',
@@ -121,7 +121,7 @@ async function buildSuseRpm(): Promise<void> {
             description: 'TUXEDO Control Center Application'
         },
         rpm: {
-            depends: [ 'tuxedo-cc-wmi >= 0.1.1' ],
+            depends: [ 'tuxedo-cc-wmi >= 0.1.4' ],
             fpm: [
                 '--after-install=./build-src/after_install.sh',
                 '--before-remove=./build-src/before_remove.sh',
