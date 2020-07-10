2cad6c2ed1a2cf0aca59f4eaf21871702a9ece71
Christian Loritz <christian.loritz@live.de>
2020-07-10 11:30:55 +0200

    copy profile from profile page

--- src/ng-app/app/profile-overview-tile/profile-overview-tile.component.ts
+++ src/ng-app/app/profile-overview-tile/profile-overview-tile.component.ts
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with TUXEDO Control Center.  If not, see <https://www.gnu.org/licenses/>.
  */
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { ITccProfile, profileImageMap } from '../../../common/models/TccProfile';
 import { UtilsService } from '../utils.service';
 import { StateService, IStateInfo } from '../state.service';
@@ -27,105 +27,111 @@ import { FormControl } from '@angular/forms';
 import { CompatibilityService } from '../compatibility.service';
 
 @Component({
-  selector: 'app-profile-overview-tile',
-  templateUrl: './profile-overview-tile.component.html',
-  styleUrls: ['./profile-overview-tile.component.scss']
+    selector: 'app-profile-overview-tile',
+    templateUrl: './profile-overview-tile.component.html',
+    styleUrls: ['./profile-overview-tile.component.scss']
 })
 export class ProfileOverviewTileComponent implements OnInit {
 
-  @Input() profile: ITccProfile;
-  @Input() hoverEffect = false;
-  @Input() isSelected = false;
-  @Input() visible = true;
-  @Input() active = false;
-  @Input() used = false;
+    @Input() profile: ITccProfile;
+    @Input() hoverEffect = false;
+    @Input() isSelected = false;
+    @Input() visible = true;
+    @Input() active = false;
+    @Input() used = false;
 
-  /**
-   * Special input to signal that it shouldn't display a profile and just
-   * display an add symbol instead.
-   *
-   * If set to true it overrules the profile input. Defaults to false.
-   */
-  @Input() addProfileTile = false;
+    /**
+     * Special input to signal that it shouldn't display a profile and just
+     * display an add symbol instead.
+     *
+     * If set to true it overrules the profile input. Defaults to false.
+     */
+    @Input() addProfileTile = false;
 
-  public showOverlay = false;
+    @Output() copyClick = new EventEmitter<string>();
 
-  public selectStateControl: FormControl;
-  public stateInputArray: IStateInfo[];
+    public showOverlay = false;
 
-  public isCustomProfile = true;
+    public selectStateControl: FormControl;
+    public stateInputArray: IStateInfo[];
 
-  constructor(
-    private utils: UtilsService,
-    private state: StateService,
-    private config: ConfigService,
-    private router: Router,
-    public compat: CompatibilityService
-  ) { }
+    public isCustomProfile = true;
 
-  ngOnInit() {
-    if (!this.addProfileTile) {
-      if (this.selectStateControl === undefined) {
-        this.selectStateControl = new FormControl(this.state.getProfileStates(this.profile.name));
-      } else {
-        this.selectStateControl.reset(this.state.getProfileStates(this.profile.name));
-      }
+    constructor(
+        private utils: UtilsService,
+        private state: StateService,
+        private config: ConfigService,
+        private router: Router,
+        public compat: CompatibilityService
+    ) { }
+
+    ngOnInit() {
+        if (!this.addProfileTile) {
+            if (this.selectStateControl === undefined) {
+                this.selectStateControl = new FormControl(this.state.getProfileStates(this.profile.name));
+            } else {
+                this.selectStateControl.reset(this.state.getProfileStates(this.profile.name));
+            }
+        }
+
+        this.stateInputArray = this.state.getStateInputs();
+        if (this.profile) {
+            this.isCustomProfile = this.config.getCustomProfileByName(this.profile.name) !== undefined;
+        }
     }
 
-    this.stateInputArray = this.state.getStateInputs();
-    if (this.profile) {
-      this.isCustomProfile = this.config.getCustomProfileByName(this.profile.name) !== undefined;
+    public getStateInputs(): IStateInfo[] {
+        return this.state.getStateInputs();
     }
-  }
 
-  public getStateInputs(): IStateInfo[] {
-    return this.state.getStateInputs();
-  }
-
-  public getSettings(): ITccSettings {
-    return this.config.getSettings();
-  }
-
-  public formatFrequency(frequency: number): string {
-    return this.utils.formatFrequency(frequency);
-  }
-
-  public activateOverlay(status: boolean): void {
-    if (!this.addProfileTile) {
-      if (status === false) {
-        this.selectStateControl.reset(this.state.getProfileStates(this.profile.name));
-      }
-      this.showOverlay = status;
+    public getSettings(): ITccSettings {
+        return this.config.getSettings();
     }
-  }
 
-  public selectProfile(): void {
-    setImmediate(() => {
-      this.router.navigate(['profile-manager', this.profile.name]);
-    });
-  }
-
-  public deleteProfile(): void {
-    this.config.deleteCustomProfile(this.profile.name);
-    this.utils.pageDisabled = false;
-  }
-
-  public saveStateSelection(): void {
-    this.utils.pageDisabled = true;
-    const profileStateAssignments: string[] = this.selectStateControl.value;
-    this.config.writeProfile(this.profile.name, this.profile, profileStateAssignments).then(success => {
-      if (success) {
-        this.selectStateControl.markAsPristine();
-      }
-      this.utils.pageDisabled = false;
-    });
-  }
-
-  public getProfileIcon(profile: ITccProfile): string {
-    if (profileImageMap.get(profile.name) !== undefined) {
-      return profileImageMap.get(profile.name);
-    } else {
-      return profileImageMap.get('custom');
+    public formatFrequency(frequency: number): string {
+        return this.utils.formatFrequency(frequency);
     }
-  }
+
+    public activateOverlay(status: boolean): void {
+        if (!this.addProfileTile) {
+            if (status === false) {
+                this.selectStateControl.reset(this.state.getProfileStates(this.profile.name));
+            }
+            this.showOverlay = status;
+        }
+    }
+
+    public selectProfile(): void {
+        setImmediate(() => {
+            this.router.navigate(['profile-manager', this.profile.name]);
+        });
+    }
+
+    public deleteProfile(): void {
+        this.config.deleteCustomProfile(this.profile.name);
+        this.utils.pageDisabled = false;
+    }
+
+    public saveStateSelection(): void {
+        this.utils.pageDisabled = true;
+        const profileStateAssignments: string[] = this.selectStateControl.value;
+        this.config.writeProfile(this.profile.name, this.profile, profileStateAssignments).then(success => {
+            if (success) {
+                this.selectStateControl.markAsPristine();
+            }
+            this.utils.pageDisabled = false;
+        });
+    }
+
+    public getProfileIcon(profile: ITccProfile): string {
+        if (profileImageMap.get(profile.name) !== undefined) {
+            return profileImageMap.get(profile.name);
+        } else {
+            return profileImageMap.get('custom');
+        }
+    }
+
+    public copyProfile() {
+        this.copyClick.emit(this.profile.name);
+    }
 }
