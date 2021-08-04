17fe6c68e75d7ad66a68e23fc2aabc733a14b607
Christoffer Sandberg <cs@tuxedo.de>
2021-08-04 18:12:08 +0200

    Fix: scalingavailablefrequencies not exist case for recent bugfix

--- src/service-app/classes/CpuWorker.ts
+++ src/service-app/classes/CpuWorker.ts
@@ -213,7 +213,7 @@ export class CpuWorker extends DaemonWorker {
         // Check settings for each core
         for (const core of this.cpuCtrl.cores) {
             if (profile.cpu.noTurbo !== true) { // Only attempt to enforce frequencies if noTurbo isn't set
-                const coreAvailableFrequencies = core.scalingAvailableFrequencies.readValue();
+                const coreAvailableFrequencies = core.scalingAvailableFrequencies.readValueNT();
                 const coreMinFreq = core.cpuinfoMinFreq.readValue();
                 const coreMaxFreq = coreAvailableFrequencies !== undefined ? coreAvailableFrequencies[0] : core.cpuinfoMaxFreq.readValue();
                 if (core.scalingMinFreq.isAvailable() && core.cpuinfoMinFreq.isAvailable()) {
