ead51f553c10033554eb7b60b4eae03220ffa444
Christoffer Sandberg <cs@tuxedo.de>
2022-01-28 21:59:14 +0100

    Profile overview tile changes
    - Add description
    - Add activate profile button
    - Add menu button (without menu)
    - Remove old "overlay"
    - Remove active profile dot

--- src/ng-app/app/profile-overview-tile/profile-overview-tile.component.ts
+++ src/ng-app/app/profile-overview-tile/profile-overview-tile.component.ts
@@ -1,5 +1,5 @@
 /*!
- * Copyright (c) 2019-2020 TUXEDO Computers GmbH <tux@tuxedocomputers.com>
+ * Copyright (c) 2019-2022 TUXEDO Computers GmbH <tux@tuxedocomputers.com>
  *
  * This file is part of TUXEDO Control Center.
  *
@@ -50,6 +50,8 @@ export class ProfileOverviewTileComponent implements OnInit {
      * If set to true it overrules the profile input. Defaults to false.
      */
     @Input() addProfileTile = false;
+
+    @Input() showDetails = false;
 
     @Output() copyClick = new EventEmitter<string>();
 
@@ -125,6 +127,10 @@ export class ProfileOverviewTileComponent implements OnInit {
             }
             this.showOverlay = status;
         }
+    }
+
+    public activateProfile(): void {
+        this.tccDBus.setTempProfile(this.profile.name);
     }
 
     public selectProfile(): void {
