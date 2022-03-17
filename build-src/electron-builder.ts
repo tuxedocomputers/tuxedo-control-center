157e1055cacec8bbca0ac5be44da6f9815a49901
Christoffer Sandberg <cs@tuxedo.de>
2022-03-17 18:46:01 +0100

    Update rpm pkg dependency + add alternative
    - Add alternative dependency for fedora rpms

--- build-src/electron-builder.ts
+++ build-src/electron-builder.ts
@@ -119,7 +119,7 @@ async function buildSuseRpm(): Promise<void> {
             description: 'TUXEDO Control Center Application'
         },
         rpm: {
-            depends: [ 'tuxedo-keyboard >= 3.0.6', 'libappindicator3-1' ],
+            depends: [ 'tuxedo-keyboard >= 3.0.6', '(libayatana-appindicator3-1 or libappindicator)' ],
             fpm: [
                 '--after-install=./build-src/after_install.sh',
                 '--before-remove=./build-src/before_remove.sh',
