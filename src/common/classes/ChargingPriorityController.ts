9218e944c102bc0638b307befe6d00d73ad1ee81
Christoffer Sandberg <cs@tuxedo.de>
2022-11-30 18:18:47 +0100

    Add charging sysfs controller interfaces

---
+++ src/common/classes/ChargingPriorityController.ts
@@ -0,0 +1,31 @@
+/*!
+ * Copyright (c) 2022 TUXEDO Computers GmbH <tux@tuxedocomputers.com>
+ *
+ * This file is part of TUXEDO Control Center.
+ *
+ * TUXEDO Control Center is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TUXEDO Control Center is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TUXEDO Control Center.  If not, see <https://www.gnu.org/licenses/>.
+ */
+import * as path from 'path';
+import { SysFsPropertyString, SysFsPropertyStringList } from './SysFsProperties';
+import { SysFsController } from './SysFsController';
+
+export class ChargingPriorityController extends SysFsController {
+
+    constructor(public readonly basePath: string) {
+        super();
+    }
+
+    readonly chargingPriosAvailable = new SysFsPropertyStringList(path.join(this.basePath, 'charging_prios_available'));
+    readonly chargingPrio = new SysFsPropertyString(path.join(this.basePath, 'charging_prio'));
+}
