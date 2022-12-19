3382a8c09814d8f7f73a8f6d23664042b2426e30
Christoffer Sandberg <cs@tuxedo.de>
2022-12-19 11:38:33 +0100

    Add labels and description placeholders per profile/priority

--- src/ng-app/app/charging-settings/charging-settings.component.ts
+++ src/ng-app/app/charging-settings/charging-settings.component.ts
@@ -40,9 +40,28 @@ export class ChargingSettingsComponent implements OnInit, OnDestroy {
     private updateInterval = 1000;
     private timeout;
 
+    public chargingProfileLabels: Map<string, string> = new Map();
+    public chargingProfileDescriptions: Map<string, string> = new Map();
+    public chargingPriorityLabels: Map<string, string> = new Map();
+    public chargingPriorityDescriptions: Map<string, string> = new Map();
+
     constructor(
         private tccdbus: TccDBusClientService
-    ) { }
+    ) {
+        this.chargingProfileLabels.set('high_capacity', $localize `:@@chargingProfileHighCapacityLabel:High capacity`);
+        this.chargingProfileLabels.set('balanced', $localize `:@@chargingProfileBalancedLabel:Balanced`);
+        this.chargingProfileLabels.set('stationary', $localize `:@@chargingProfileStationaryLabel:Stationary`);
+
+        this.chargingProfileDescriptions.set('high_capacity', $localize `:@@chargingProfileHighCapacityDescription:Optimize for charging speed and maximum capacity.`);
+        this.chargingProfileDescriptions.set('balanced', $localize `:@@chargingProfileBalancedDescription:Balance between battery capacity and battery lifetime. Charging speed and peak capacity reduced.`);
+        this.chargingProfileDescriptions.set('stationary', $localize `:@@chargingProfileStationaryDescription:Further reduced charging speed and peak capacity. Suitable when using external power supply almost extensively.`);
+
+        this.chargingPriorityLabels.set('charge_battery', $localize `:@@chargingPriorityChargeBatteryLabel:Charge battery`);
+        this.chargingPriorityLabels.set('performance', $localize `:@@chargingPriorityPerformanceLabel:Performance`);
+
+        this.chargingPriorityDescriptions.set('charge_battery', $localize `:@@chargingPriorityChargeBatteryDescription:Prioritizes battery charging at the expense of performance.`);
+        this.chargingPriorityDescriptions.set('performance', $localize `:@@chargingPriorityPerformanceDescription:Prioritizes performance at the expense of battery charge speed.`);
+    }
 
     ngOnInit() {
         this.periodicUpdate().then(() => {
