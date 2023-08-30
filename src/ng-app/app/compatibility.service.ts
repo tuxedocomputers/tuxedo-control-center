2cc577a03f1ba44364e1d9335de96ba0040199e4
tuxedo.der <d.erler@tuxedocomputers.com>
2023-08-30 19:30:43 +0200

    changing compatibility message, formatting compatibility service

--- src/ng-app/app/compatibility.service.ts
+++ src/ng-app/app/compatibility.service.ts
@@ -16,192 +16,200 @@
  * You should have received a copy of the GNU General Public License
  * along with TUXEDO Control Center.  If not, see <https://www.gnu.org/licenses/>.
  */
-import { Injectable } from '@angular/core';
-import { ScalingDriver } from '../../common/classes/LogicalCpuController';
-import { DMIController } from '../../common/classes/DMIController';
-import { SysFsService } from './sys-fs.service';
-import { TccDBusClientService } from './tcc-dbus-client.service';
-import { IdGpuInfo, IiGpuInfo } from 'src/common/models/TccGpuValues';
+import { Injectable } from "@angular/core";
+import { ScalingDriver } from "../../common/classes/LogicalCpuController";
+import { DMIController } from "../../common/classes/DMIController";
+import { SysFsService } from "./sys-fs.service";
+import { TccDBusClientService } from "./tcc-dbus-client.service";
+import { IdGpuInfo, IiGpuInfo } from "src/common/models/TccGpuValues";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: "root",
 })
 export class CompatibilityService {
+    private hasAquarisValue: boolean;
 
-  private hasAquarisValue: boolean;
+    constructor(
+        private tccDbus: TccDBusClientService,
+        private sysfs: SysFsService
+    ) {
+        // TODO: Manual read until general device id get merged
+        const dmi = new DMIController("/sys/class/dmi/id");
+        const deviceName = dmi.productSKU.readValueNT();
+        const boardVendor = dmi.boardVendor.readValueNT();
+        const chassisVendor = dmi.chassisVendor.readValueNT();
+        const sysVendor = dmi.sysVendor.readValueNT();
+        let showAquarisMenu;
+        const isTuxedo =
+            (boardVendor !== undefined &&
+                boardVendor.toLowerCase().includes("tuxedo")) ||
+            (chassisVendor !== undefined &&
+                chassisVendor.toLowerCase().includes("tuxedo")) ||
+            (sysVendor !== undefined &&
+                sysVendor.toLowerCase().includes("tuxedo"));
 
-  constructor(private tccDbus: TccDBusClientService, private sysfs: SysFsService) {
-    // TODO: Manual read until general device id get merged
-    const dmi = new DMIController('/sys/class/dmi/id');
-    const deviceName = dmi.productSKU.readValueNT();
-    const boardVendor = dmi.boardVendor.readValueNT();
-    const chassisVendor = dmi.chassisVendor.readValueNT();
-    const sysVendor = dmi.sysVendor.readValueNT();
-    let showAquarisMenu;
-    const isTuxedo = (boardVendor !== undefined && boardVendor.toLowerCase().includes('tuxedo')) ||
-                     (chassisVendor !== undefined && chassisVendor.toLowerCase().includes('tuxedo')) ||
-                     (sysVendor !== undefined && sysVendor.toLowerCase().includes('tuxedo'));
-
-    if (isTuxedo) {
-        if (deviceName !== undefined &&
-            (deviceName === 'STELLARIS1XI04' ||
-             deviceName === 'STEPOL1XA04' ||
-             deviceName === 'STELLARIS1XI05')) {
-        showAquarisMenu = true;
-      } else {
-        showAquarisMenu = false;
-      }
-    } else {
-      showAquarisMenu = true;
-    }
-    this.hasAquarisValue = showAquarisMenu;
-  }
-
-  get hasFanInfo(): boolean {
-    return this.hasFanControl;
-  }
-
-  get hasCpuPower(): boolean {
-    const { cpuPower } = this.tccDbus;
-    const { value: cpuPowerValue } = cpuPower;
-
-    const powerDrawDefined =
-        typeof cpuPowerValue?.powerDraw !== "undefined";
-
-    return (
-        powerDrawDefined &&
-        cpuPowerValue.powerDraw > 0 &&
-        this.tccDbus.tuxedoWmiAvailable.value
-    );
-  }
-
-  get hasDGpuPowerDraw(): boolean {
-    const dGpuPowerDraw = this.tccDbus.dGpuInfo?.value?.powerDraw;
-
-    if (dGpuPowerDraw !== undefined) {
-        return dGpuPowerDraw > 0 && this.tccDbus.tuxedoWmiAvailable.value;
+        if (isTuxedo) {
+            if (
+                deviceName !== undefined &&
+                (deviceName === "STELLARIS1XI04" ||
+                    deviceName === "STEPOL1XA04" ||
+                    deviceName === "STELLARIS1XI05")
+            ) {
+                showAquarisMenu = true;
+            } else {
+                showAquarisMenu = false;
+            }
+        } else {
+            showAquarisMenu = true;
+        }
+        this.hasAquarisValue = showAquarisMenu;
     }
 
-    return false;
-  }
-
-  get hasIGpuPowerDraw(): boolean {
-    const iGpuPowerDraw = this.tccDbus.iGpuInfo?.value?.powerDraw;
-
-    if (iGpuPowerDraw !== undefined) {
-        return iGpuPowerDraw >= 0 && this.tccDbus.tuxedoWmiAvailable.value;
+    get hasFanInfo(): boolean {
+        return this.hasFanControl;
     }
 
-    return false;
-  }
+    get hasCpuPower(): boolean {
+        const { cpuPower } = this.tccDbus;
+        const { value: cpuPowerValue } = cpuPower;
 
-  get hasDGpuFreq(): boolean {
-    const dGpuInfo: IdGpuInfo | undefined = this.tccDbus.dGpuInfo?.value;
-    if (!dGpuInfo) {
+        const powerDrawDefined =
+            typeof cpuPowerValue?.powerDraw !== "undefined";
+
+        return (
+            powerDrawDefined &&
+            cpuPowerValue.powerDraw > 0 &&
+            this.tccDbus.tuxedoWmiAvailable.value
+        );
+    }
+
+    get hasDGpuPowerDraw(): boolean {
+        const dGpuPowerDraw = this.tccDbus.dGpuInfo?.value?.powerDraw;
+
+        if (dGpuPowerDraw !== undefined) {
+            return dGpuPowerDraw > 0 && this.tccDbus.tuxedoWmiAvailable.value;
+        }
+
         return false;
     }
-    const { coreFrequency, maxCoreFrequency } = dGpuInfo;
-    return (
-        coreFrequency !== undefined &&
-        coreFrequency >= 0 &&
-        maxCoreFrequency !== undefined &&
-        maxCoreFrequency >= 0 &&
-        this.tccDbus.tuxedoWmiAvailable.value
-    );
-  }
 
-  get hasIGpuFreq(): boolean {
-    const iGpuInfo: IiGpuInfo | undefined = this.tccDbus.iGpuInfo?.value;
-    if (!iGpuInfo) {
+    get hasIGpuPowerDraw(): boolean {
+        const iGpuPowerDraw = this.tccDbus.iGpuInfo?.value?.powerDraw;
+
+        if (iGpuPowerDraw !== undefined) {
+            return iGpuPowerDraw >= 0 && this.tccDbus.tuxedoWmiAvailable.value;
+        }
+
         return false;
     }
-    const { coreFrequency, maxCoreFrequency } = iGpuInfo;
-    return (
-        coreFrequency !== undefined &&
-        coreFrequency >= 0 &&
-        maxCoreFrequency !== undefined &&
-        maxCoreFrequency >= 0 &&
-        this.tccDbus.tuxedoWmiAvailable.value
-    );
-  }
 
-  get hasIGpuTemp(): boolean {
-    const iGpuInfo = this.tccDbus.iGpuInfo?.value;
-    const temp = iGpuInfo?.temp ?? -1;
-    return temp > 0 && this.tccDbus.tuxedoWmiAvailable.value;
-  }
+    get hasDGpuFreq(): boolean {
+        const dGpuInfo: IdGpuInfo | undefined = this.tccDbus.dGpuInfo?.value;
+        if (!dGpuInfo) {
+            return false;
+        }
+        const { coreFrequency, maxCoreFrequency } = dGpuInfo;
+        return (
+            coreFrequency !== undefined &&
+            coreFrequency >= 0 &&
+            maxCoreFrequency !== undefined &&
+            maxCoreFrequency >= 0 &&
+            this.tccDbus.tuxedoWmiAvailable.value
+        );
+    }
 
-  // hasFanControl==true implies hasFanInfo==true, but not the other way around
-  get hasFanControl(): boolean {
-    /*const dmi = new DMIController('/sys/class/dmi/id');
+    get hasIGpuFreq(): boolean {
+        const iGpuInfo: IiGpuInfo | undefined = this.tccDbus.iGpuInfo?.value;
+        if (!iGpuInfo) {
+            return false;
+        }
+        const { coreFrequency, maxCoreFrequency } = iGpuInfo;
+        return (
+            coreFrequency !== undefined &&
+            coreFrequency >= 0 &&
+            maxCoreFrequency !== undefined &&
+            maxCoreFrequency >= 0 &&
+            this.tccDbus.tuxedoWmiAvailable.value
+        );
+    }
+
+    get hasIGpuTemp(): boolean {
+        const iGpuInfo = this.tccDbus.iGpuInfo?.value;
+        const temp = iGpuInfo?.temp ?? -1;
+        return temp > 0 && this.tccDbus.tuxedoWmiAvailable.value;
+    }
+
+    // hasFanControl==true implies hasFanInfo==true, but not the other way around
+    get hasFanControl(): boolean {
+        /*const dmi = new DMIController('/sys/class/dmi/id');
     const boardName = dmi.boardName.readValueNT();
     // when adding or removing devices here don't forget to also alter getFanControlStatus() from FanControlWorker.ts from tccd
     if (boardName === "GMxRGxx") {
       return false;
     }*/
-    return this.tccDbus.tuxedoWmiAvailable.value && this.tccDbus.fanData.value.cpu.temp.data.value > 1;
-  }
-
-  get fanControlCompatibilityMessage(): string {
-    return $localize `:@@compatibilityMessageNoFanControl:This feature is not supported on your model.`;
-  }
-
-  get cpuPowerCompatibilityMessage(): string {
-    return $localize `:@@compatibilityMessageNoCpuPower:This feature is not supported on your model.`;
-  }
-
-  get gpuPowerCompatibilityMessage(): string {
-    return $localize `:@@compatibilityMessageNoGpuPower:This feature is not supported on your model.`;
-  }
-
-  get gpuFreqCompatibilityMessage(): string {
-    return $localize `:@@compatibilityMessageNoGpuFreq:This feature is not supported on your model.`;
-  }
-
-  get hasWebcamControl(): boolean {
-    return this.tccDbus.tuxedoWmiAvailable.value && this.tccDbus.webcamSWAvailable.value;
-  }
-
-  get webcamControlCompatibilityMessage(): string {
-    return $localize `:@@compatibilityMessageNoWebcamSwitch:This feature is not supported on your model.`;
-  }
-
-  get hasODMProfileControl(): boolean {
-      return this.tccDbus.tuxedoWmiAvailable.value &&
-        this.tccDbus.odmProfilesAvailable.value !== undefined &&
-        this.tccDbus.odmProfilesAvailable.value.length > 0;
-  }
-
-  get hasODMPowerLimitControl(): boolean {
-      return this.tccDbus.tuxedoWmiAvailable.value &&
-        this.tccDbus.odmPowerLimits.value !== undefined &&
-        this.tccDbus.odmPowerLimits.value.length > 0;
-  }
-
-  get uwLEDOnlyMode(): boolean {
-      return this.hasODMPowerLimitControl &&
-        this.hasODMProfileControl;
-  }
-
-  get tccDbusAvailable() {
-    return this.tccDbus.available;
-  }
-
-  get hasAquaris() {
-    return this.hasAquarisValue;
-  }
-
-  /**
-  * Condition where max freq workaround is applicable
-  * (aka max freq missing regulated through boost flag)
-  */
-  get hasMissingMaxFreqBoostWorkaround() {
-    if (this.sysfs.generalCpuInfo.value !== undefined && this.sysfs.logicalCoreInfo.value !== undefined) {
-        const boost = this.sysfs.generalCpuInfo.value.boost;
-        const scalingDriver = this.sysfs.logicalCoreInfo.value[0].scalingDriver;
-        return boost !== undefined && scalingDriver === ScalingDriver.acpi_cpufreq;
-    } else {
-        return false;
+        return (
+            this.tccDbus.tuxedoWmiAvailable.value &&
+            this.tccDbus.fanData.value.cpu.temp.data.value > 1
+        );
     }
-  }
+
+    get compatibilityMessage(): string {
+        return $localize`:@@compatibilityNotAvailable:This feature is currently not available.`;
+    }
+
+    get hasWebcamControl(): boolean {
+        return (
+            this.tccDbus.tuxedoWmiAvailable.value &&
+            this.tccDbus.webcamSWAvailable.value
+        );
+    }
+
+    get hasODMProfileControl(): boolean {
+        return (
+            this.tccDbus.tuxedoWmiAvailable.value &&
+            this.tccDbus.odmProfilesAvailable.value !== undefined &&
+            this.tccDbus.odmProfilesAvailable.value.length > 0
+        );
+    }
+
+    get hasODMPowerLimitControl(): boolean {
+        return (
+            this.tccDbus.tuxedoWmiAvailable.value &&
+            this.tccDbus.odmPowerLimits.value !== undefined &&
+            this.tccDbus.odmPowerLimits.value.length > 0
+        );
+    }
+
+    get uwLEDOnlyMode(): boolean {
+        return this.hasODMPowerLimitControl && this.hasODMProfileControl;
+    }
+
+    get tccDbusAvailable() {
+        return this.tccDbus.available;
+    }
+
+    get hasAquaris() {
+        return this.hasAquarisValue;
+    }
+
+    /**
+     * Condition where max freq workaround is applicable
+     * (aka max freq missing regulated through boost flag)
+     */
+    get hasMissingMaxFreqBoostWorkaround() {
+        if (
+            this.sysfs.generalCpuInfo.value !== undefined &&
+            this.sysfs.logicalCoreInfo.value !== undefined
+        ) {
+            const boost = this.sysfs.generalCpuInfo.value.boost;
+            const scalingDriver =
+                this.sysfs.logicalCoreInfo.value[0].scalingDriver;
+            return (
+                boost !== undefined &&
+                scalingDriver === ScalingDriver.acpi_cpufreq
+            );
+        } else {
+            return false;
+        }
+    }
 }
