1aa9ae779e073356c97dcfd266007ec3e4626018
tuxedo.der <d.erler@tuxedocomputers.com>
2026-01-13 18:03:20 +0100

    adding ib max gen10 amd prime-select fix

--- src/service-app/classes/PrimeWorker.ts
+++ src/service-app/classes/PrimeWorker.ts
@@ -19,20 +19,30 @@
 
 import * as fs from 'node:fs';
 import { delay, execCommandAsync } from '../../common/classes/Utils';
+import { TUXEDODevice } from '../../common/models/DefaultProfiles';
 import { DaemonWorker } from './DaemonWorker';
 import type { TuxedoControlCenterDaemon } from './TuxedoControlCenterDaemon';
 
+const fsp: typeof import('fs').promises = require('fs').promises;
+
 export class PrimeWorker extends DaemonWorker {
-    private primeSupported: Boolean;
+    private tuxedoDevice: TUXEDODevice;
+    private isDisplayConnectedToNvidia: boolean;
 
     constructor(tccd: TuxedoControlCenterDaemon) {
         super(10000, 'PrimeWorker', tccd);
+        this.tuxedoDevice = this.tccd.identifyDevice();
     }
 
     public async onStart(): Promise<void> {
         // not instantly setting prime status in onStart() because requires_offloading only gets updated after some delay
         // checking it directly in onStart() will result in getting a wrong state
         await delay(2000);
+
+        if (this.tuxedoDevice === TUXEDODevice.IBM15A10) {
+            this.isDisplayConnectedToNvidia = await this.getDisplayConnectedToNvidia();
+        }
+
         this.setPrimeStatus();
     }
 
@@ -41,28 +51,61 @@ export class PrimeWorker extends DaemonWorker {
         this.setPrimeStatus();
     }
 
+    public async onExit(): Promise<void> {}
+
     private async setPrimeStatus(): Promise<void> {
         const primeSupported: boolean = await this.checkPrimeSupported();
 
         if (primeSupported) {
             this.tccd.dbusData.primeState = JSON.stringify(await this.checkPrimeStatus());
-            this.primeSupported = primeSupported;
         }
         if (!primeSupported) {
             this.tccd.dbusData.primeState = JSON.stringify('-1');
         }
     }
 
-    public async onExit(): Promise<void> {}
-
     // only supporting gpu switch on systems which can use prime-select since primary focus is Tuxdeo OS and Ubuntu
     // other operating systems may handle this differently and thus can't easily be supported
     private async checkPrimeSupported(): Promise<boolean> {
+        if (this.tuxedoDevice === TUXEDODevice.IBM15A10) {
+            return !this.isDisplayConnectedToNvidia;
+        }
+
         const offloadingStatus: boolean = fs.existsSync('/var/lib/ubuntu-drivers-common/requires_offloading') === true;
 
         const primeAvailable: string = (await execCommandAsync('which prime-select | cat')).toString().trim();
 
         return offloadingStatus && !!primeAvailable;
+    }
+
+    private async getDisplayConnectedToNvidia(): Promise<boolean> {
+        try {
+            const drmPath: string = '/sys/class/drm/';
+            const drmDirs: string[] = await fsp.readdir(drmPath);
+            const edpDirs: string[] = drmDirs.filter((dir: string) => dir.includes('eDP'));
+
+            for (const edpDir of edpDirs) {
+                const edpPath: string = `${drmPath}/${edpDir}`;
+                const vendorPath: string = `${edpPath}/device/device/vendor`;
+                const statusPath: string = `${edpPath}/status`;
+
+                try {
+                    const vendorId: string = (await fsp.readFile(vendorPath, 'utf-8')).trim();
+                    const status: string = (await fsp.readFile(statusPath, 'utf-8')).trim();
+
+                    if (vendorId.toLowerCase() === '0x10de' && status === 'connected') {
+                        return true;
+                    }
+                } catch (err: unknown) {
+                    console.error(`PrimeWorker: getDisplayConnectedToNvidia failed => ${err}`);
+                }
+            }
+
+            return false;
+        } catch (err: unknown) {
+            console.error(`PrimeWorker: getDisplayConnectedToNvidia failed => ${err}`);
+            return false;
+        }
     }
 
     private async checkPrimeStatus(): Promise<string> {
